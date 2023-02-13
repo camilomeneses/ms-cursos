b9d1d67be6b694f42de05e1dc67703fd75c44b69
Camilo Meneses <kamilomeneses@gmail.com>
2023-02-13 10:17:23 -0500

    MS-CURSOS: impl metodos de comunicacion con ms-usuarios en el controller

--- src/main/java/dev/camilo/mscursos/controllers/CursoController.java
+++ src/main/java/dev/camilo/mscursos/controllers/CursoController.java
@@ -1,7 +1,9 @@
 package dev.camilo.mscursos.controllers;
 
+import dev.camilo.mscursos.models.Usuario;
 import dev.camilo.mscursos.models.entities.Curso;
 import dev.camilo.mscursos.services.CursoService;
+import feign.FeignException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,36 +11,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping( "/" )
 public class CursoController {
 
   private final CursoService service;
 
   @GetMapping
-  public ResponseEntity<List<Curso>> listar(){
+  public ResponseEntity <List <Curso>> listar() {
 
-    return ResponseEntity.ok(service.listar());
+    return ResponseEntity.ok( service.listar() );
   }
 
-  @GetMapping("/{id}")
-  public ResponseEntity<?> detalle( @PathVariable Long id ){
+  @GetMapping( "/{id}" )
+  public ResponseEntity <?> detalle( @PathVariable Long id ) {
 
     Optional <Curso> cursoOptional = service.porId( id );
-    if(cursoOptional.isPresent()){
-      return ResponseEntity.ok(cursoOptional.get());
+    if ( cursoOptional.isPresent() ) {
+      return ResponseEntity.ok( cursoOptional.get() );
     }
     return ResponseEntity.notFound().build();
   }
 
   @PostMapping
-  public ResponseEntity<?> crear(@Valid @RequestBody Curso curso, BindingResult result){
+  public ResponseEntity <?> crear( @Valid @RequestBody Curso curso, BindingResult result ) {
 
     if ( result.hasErrors() ) {
       return validar( result );
@@ -47,14 +46,14 @@ public class CursoController {
     return ResponseEntity.status( HttpStatus.CREATED ).body( cursoDB );
   }
 
-  @PutMapping("/{id}")
-  public ResponseEntity<?> editar(@Valid @PathVariable Long id, @RequestBody Curso curso, BindingResult result){
+  @PutMapping( "/{id}" )
+  public ResponseEntity <?> editar( @Valid @PathVariable Long id, @RequestBody Curso curso, BindingResult result ) {
 
     if ( result.hasErrors() ) {
       return validar( result );
     }
     Optional <Curso> cursoOptional = service.porId( id );
-    if(cursoOptional.isPresent()){
+    if ( cursoOptional.isPresent() ) {
       Curso cursoDB = cursoOptional.get();
       cursoDB.setNombre( curso.getNombre() );
       return ResponseEntity.status( HttpStatus.CREATED ).body( service.guardar( cursoDB ) );
@@ -62,17 +61,18 @@ public class CursoController {
     return ResponseEntity.notFound().build();
   }
 
-  @DeleteMapping("/{id}")
-  public ResponseEntity<?> eliminar(@PathVariable Long id){
+  @DeleteMapping( "/{id}" )
+  public ResponseEntity <?> eliminar( @PathVariable Long id ) {
 
     Optional <Curso> cursoOptional = service.porId( id );
-    if(cursoOptional.isPresent()){
+    if ( cursoOptional.isPresent() ) {
       service.eliminar( id );
       return ResponseEntity.noContent().build();
     }
     return ResponseEntity.notFound().build();
   }
 
+  // metodos privados de controlador
   private static ResponseEntity <Map <String, String>> validar( BindingResult result ) {
 
     Map <String, String> errores = new HashMap <>();
@@ -80,5 +80,72 @@ public class CursoController {
       errores.put( err.getField(), "El campo ".concat( err.getField() ).concat( " " ).concat( err.getDefaultMessage() ) );
     } );
     return ResponseEntity.badRequest().body( errores );
+  }
+
+  // metodos para cliente ms-usuarios
+  @PutMapping( "/asignar-usuario/{cursoId}" )
+  public ResponseEntity <?> asignarUsuario( @RequestBody Usuario usuario, @PathVariable Long cursoId ) {
+
+    /* Creamos la varible para traer el Optional del service*/
+    Optional <Usuario> usuarioOptional;
+
+    /* Teniendo en cuenta algun error de no encontrar el usuario por el id o exista un error por delay o otro motivo
+    * con el ms-usuarios lanzara una exception personalizada  */
+    try {
+      usuarioOptional = service.asignarUsuario( usuario, cursoId );
+    } catch ( FeignException e ) {
+      return ResponseEntity.status( HttpStatus.NOT_FOUND )
+          .body( Collections.singletonMap( "error", "No existe el usuario por el id o error en la comunicacion".concat( e.getMessage() ) ) );
+    }
+
+    /* si todo sale bien entonces, respondemos con el usuario asignado al curso sino con un estado no encontrado */
+    if ( usuarioOptional.isPresent() ) {
+      return ResponseEntity.status( HttpStatus.CREATED ).body( usuarioOptional.get() );
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+  @PostMapping( "/crear-usuario/{cursoId}" )
+  public ResponseEntity <?> crearUsuario( @RequestBody Usuario usuario, @PathVariable Long cursoId ) {
+
+    /* Creamos la varible para traer el Optional del service*/
+    Optional <Usuario> usuarioOptional;
+
+    /* Teniendo en cuenta algun error por delay o otro motivo
+     * con el ms-usuarios lanzara una exception personalizada  */
+    try {
+      usuarioOptional = service.crearUsuario( usuario, cursoId );
+    } catch ( FeignException e ) {
+      return ResponseEntity.status( HttpStatus.NOT_FOUND )
+          .body( Collections.singletonMap( "error", "No se pudo crear el usuario o error en la comunicacion".concat( e.getMessage() ) ) );
+    }
+
+    /* si todo sale bien entonces, respondemos con el usuario asignado al curso sino con un estado no encontrado */
+    if ( usuarioOptional.isPresent() ) {
+      return ResponseEntity.status( HttpStatus.CREATED ).body( usuarioOptional.get() );
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+  @DeleteMapping( "/desasignar-usuario/{cursoId}" )
+  public ResponseEntity <?> desasignarUsuario( @RequestBody Usuario usuario, @PathVariable Long cursoId ) {
+
+    /* Creamos la varible para traer el Optional del service*/
+    Optional <Usuario> usuarioOptional;
+
+    /* Teniendo en cuenta algun error de no encontrar el usuario por el id o exista un error por delay o otro motivo
+     * con el ms-usuarios lanzara una exception personalizada  */
+    try {
+      usuarioOptional = service.desasignarUsuario( usuario, cursoId );
+    } catch ( FeignException e ) {
+      return ResponseEntity.status( HttpStatus.NOT_FOUND )
+          .body( Collections.singletonMap( "error", "No existe el usuario por el id o error en la comunicacion".concat( e.getMessage() ) ) );
+    }
+
+    /* si todo sale bien entonces, respondemos con el usuario asignado al curso sino con un estado no encontrado */
+    if ( usuarioOptional.isPresent() ) {
+      return ResponseEntity.status( HttpStatus.OK ).body( usuarioOptional.get() );
+    }
+    return ResponseEntity.notFound().build();
   }
 }
