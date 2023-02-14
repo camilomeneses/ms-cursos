e755217786c604a3764bc40ee60a8149602e6b35
Camilo Meneses <kamilomeneses@gmail.com>
2023-02-13 22:10:57 -0500

    MS-USUARIOS: add cliente HTTP Feign eliminar usuario de ms-cursos

--- src/main/java/dev/camilo/mscursos/MsCursosApplication.java
+++ src/main/java/dev/camilo/mscursos/MsCursosApplication.java
@@ -1,11 +1,14 @@
 package dev.camilo.mscursos;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
-@EnableFeignClients
 @SpringBootApplication
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class MsCursosApplication {
 
   public static void main( String[] args ) {
